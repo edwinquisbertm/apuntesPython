b474eb7f3d4f2e42272bec31dccf8fcd4da54954
edwinquisbertm <e.quisbert@umss.edu.bo>
2024-08-15 00:42:11 -0400

    Curso de Python: PIP y Entornos Virtuales

--- 12_entornoTrabajo/web-server/main.py
+++ 12_entornoTrabajo/web-server/main.py
@@ -1,4 +1,26 @@
 import store
+from fastapi import FastAPI # Importamos la librería fastAPI y la renombramos como fastAPI para poder usarla
+from fastapi.responses import HTMLResponse
+
+app = FastAPI() # Creamos una instancia de FastAPI
+
+@app.get('/') # Definimos una ruta para el método GET
+def get_list():
+    return [1, 2, 3, 4, 5]
+
+@app.get('/contact', response_class = HTMLResponse) # Definimos una ruta para el método GET y especificamos que la respuesta será HTML
+def get_list():
+    return """
+        <html>
+            <head>
+                <title>Contact</title>
+            </head>
+            <body>
+                <h1>Contact</h1>
+                <p>Send us an email to <a href="mailto: ""> </a></p>
+            </body>
+        </html>
+    """
 
 def run():
     store.get_categories()
